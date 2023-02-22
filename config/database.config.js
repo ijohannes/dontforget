11cdbb444886172a4be3b73650948ba26ec7c92d
nestor triana <nestor.trianat@autonoma.edu.co>
2023-02-21 20:06:21 -0500

    se configura acceso a la bd de mongo

--- config/database.config.js
+++ config/database.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    url: 'mongodb+srv://ingeniero:Hola123@cluster0.otlgow9.mongodb.net/demo?retryWrites=true&w=majority',
+    url: 'mongodb+srv://uam-app:dontforget_app@cluster0.6cmvgjy.mongodb.net/dontforget?retryWrites=true&w=majority',
     options: {
         useNewUrlParser: true,
         useUnifiedTopology: true
