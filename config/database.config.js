c0df6e824e0f2b610d13821f633064a96d15b612
nestor triana <nestor.trianat@autonoma.edu.co>
2023-02-21 19:43:27 -0500

    primer commit - se crea el listado de directorios

---
+++ config/database.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    url: 'mongodb+srv://ingeniero:Hola123@cluster0.otlgow9.mongodb.net/demo?retryWrites=true&w=majority',
+    options: {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+}
+
+// mongodb+srv://ingeniero:Hola123@cluster0.otlgow9.mongodb.net/demo?retryWrites=true&w=majority
